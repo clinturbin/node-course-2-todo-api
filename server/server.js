1725cf74c28fddf76f11a3f4fe50a9f5d0412c8a
clinturbin <clinturbin@gmail.com>
2019-01-01 09:42:49 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -24,6 +25,23 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (error) => {
         res.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
     });
 });
 
