78b8baf76611a8d918a13c43d53778e15e754b46
clinturbin <clinturbin@gmail.com>
2019-01-01 10:14:16 -0500

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,16 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const{app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
-    {text: 'First test todo'},
-    {text: 'Second test todo'},
+    {_id: new ObjectID(), text: 'First test todo'},
+    {_id: new ObjectID(), text: 'Second test todo'},
 ];
 
 beforeEach((done) => {
-    Todo.remove({}).then(() => {
+    // Todo.remove({}).then(() => {
+    Todo.deleteMany({}).then(() => {
         return Todo.insertMany(todos);
     }).then(() => done());
 });
@@ -65,6 +67,33 @@ describe("GET /todos", () => {
         .expect((res) => {
             expect(res.body.todos.length).toBe(2);
         })
+        .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+    it('should return a 404 if todo not found', (done) => {
+        let hexId = new ObjectID().toHexString();
+        // make sure you get a 404 back
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return a 404 for non object ID\'s', (done) => {
+        // make sure you get a 404 back
+        request(app)
+        .get(`/todos/1234abcd`)
+        .expect(404)
         .end(done);
     });
 });
