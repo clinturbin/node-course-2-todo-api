fb98976b48bb769a1b49ba3f5935c0f303f091f6
clinturbin <clinturbin@gmail.com>
2019-01-05 18:30:35 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // let message = 'I am user number 3';
 // let hash = SHA256(message).toString();
@@ -31,14 +32,29 @@ const jwt = require('jsonwebtoken');
 // jwt.sign takes the object and signs it, creates that hash and returns that token value
 // jwt.verify takes the token and the secret and makes sure the data was not manipulated
 
-let data = {
-    id: 10
-};
+// let data = {
+//     id: 10
+// };
 
-let secret = '123abc';
+// let secret = '123abc';
 
-let token = jwt.sign(data, secret);
-console.log('Token: ' + token);
+// let token = jwt.sign(data, secret);
+// console.log('Token: ' + token);
 
-let decoded = jwt.verify(token, secret);
-console.log('Decoded: ' + JSON.stringify(decoded));
+// let decoded = jwt.verify(token, secret);
+// console.log('Decoded: ' + JSON.stringify(decoded));
+
+//---------------------------------------------------
+let password = '123abc';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+let hashedPassword = '$2a$10$mpzb.DoOqC0XPzGSFdaXFe6KPv5a/zNyx3j9KvnnnyLWJykK53bAO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+console.log(res);
+});
