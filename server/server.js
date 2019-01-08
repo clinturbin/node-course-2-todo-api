d2101c265200da9fb452f3b319ed9a66c41cee29
clinturbin <clinturbin@gmail.com>
2019-01-08 11:03:25 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -98,9 +98,22 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth', token).send(user)
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
+    });
+});
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
